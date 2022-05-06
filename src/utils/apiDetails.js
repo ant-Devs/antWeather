473caf634944a81e89ef72b4a32643b808fa736c
David-Main <work.davidmainoo@gmail.com>
2022-05-06 13:31:51 +0000

    change api url to https

--- src/utils/apiDetails.js
+++ src/utils/apiDetails.js
@@ -1,10 +1,10 @@
 module.exports = {
 	// accuweather
-	geoLocationURL: `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${"XzPAPcfHHboIjNxp8QUMJxrFWak91weM"}&q=`,
+	geoLocationURL: `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${"XzPAPcfHHboIjNxp8QUMJxrFWak91weM"}&q=`,
 	locationsURL:
-		"http://dataservice.accuweather.com/locations/v1/cities/search",
+		"https://dataservice.accuweather.com/locations/v1/cities/search",
 	weatherURL:
-		"http://dataservice.accuweather.com/currentconditions/v1/",
+		"https://dataservice.accuweather.com/currentconditions/v1/",
 	apiKey: "DGeolksGjfzsea3W7XxqY6lTH3vRtZmX",
 	apiKey2: "XzPAPcfHHboIjNxp8QUMJxrFWak91weM",
 
@@ -12,5 +12,5 @@ module.exports = {
 	geoURL:
 		"https://geo.ipify.org/api/v2/country,city?apiKey=at_uIaUhrlyjngrcBACfwfEjFnf8GaF1",
 
-	ipURL: "http://ip-api.com/json/",
+	ipURL: "https://ip-api.com/json/",
 };
